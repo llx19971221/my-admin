39c00f3c607478691f2815216b6813d1ada618fa
luoleixin <36695108+llx19971221@users.noreply.github.com>
2020-07-17 18:30:08 +0800

    更新进度

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
-    <div id="test"></div>
+    <!-- <div id="test"></div> -->
+	<div id="test2"></div>
 </template>
 <script>
 export default {
@@ -50,10 +51,64 @@ export default {
         	    .attr("class","axis")
         	    .attr("transform","translate(20,130)")
         	    .call(axis);
-        }
+        },
+
+		init2() {
+			const {$d3: d3} = this;
+			let svg = d3.select("#test2").append("svg")
+			.attr("width", 1000)
+			.attr("height", 400);
+
+			let dataArr = [30, 45, 37, 54, 55, 61, 20];
+			
+			svg.selectAll("rect")
+			.data(dataArr)
+			.enter().append("rect")
+			.attr("class", 'bar')
+			.attr("height", (num) => {
+				return num + '%'
+			})
+			.attr('width', 100)
+			.attr("x", (num, idx) => {
+				return idx * 100 + 20 * idx
+			})
+			.attr("y", (num, idx) => {
+				return 400 - num / 100 * 400;
+			})
+
+			svg.selectAll('text')
+			.data(dataArr)
+			.enter().append('text')
+			.text((n) => n)
+			.attr("class", 'test2-text')
+			.attr("x", (num, idx) => {
+				return idx * 100 + 20 * idx + 40
+			})
+			.attr("y", (num, idx) => {
+				return 400 - num / 100 * 400 + 20;
+			})
+
+
+
+		}
     },
     mounted() {
-        this.init();
+        this.init2();
     }
 }
 </script>
+<style lang="less">
+.bar {
+  fill: #0080FF;
+  stroke: black;
+  stroke-width: 5;
+  &:hover {
+	fill: red;
+  }
+}
+
+.test2-text {
+	fill: white;
+  	font-family: sans-serif
+}
+</style>
